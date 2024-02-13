20d4bab791ec4e56ebbd75e5450d7abf4910bf40
Khaled <khalegjr@gmail.com>
2024-02-12 22:44:15 -0300

    Removendo frontend subtree

--- frontend/src/main/java/dev/khaled/backendtest/dto/CredentialsDto.java
+++
@@ -1,4 +0,0 @@
-package dev.khaled.backendtest.dto;
-
-
-public record CredentialsDto(String login, char[] password) {}
--- frontend/src/main/java/dev/khaled/backendtest/dto/ErrorDto.java
+++
@@ -1,4 +0,0 @@
-package dev.khaled.backendtest.dto;
-
-public record ErrorDto(String message) {
-}
